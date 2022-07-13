4cadd8b03ff203a98cc8cc2ac5bcee334a0388ca
Leandro Lago da Silva <leandro@datacampo.com.br>
2022-07-13 18:34:51 -0300

    posts and comments

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,37 @@ import { Post } from './components/Post'
 import styles from './App.module.css'
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/lemaun.png',
+      name: 'Leandro L. Silva',
+      role: 'CTO @Datacampo',
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-07-10 22:13:15')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/leilaneluize.png',
+      name: 'Leilane Oliveira',
+      role: 'CEO @Datacampo',
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-07-11 11:58:50')
+  }
+]
+
 export default function App() {
 
   return (
@@ -14,14 +45,16 @@ export default function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author="Leandro Silva"
-            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime quibusdam assumenda tempore ipsam ad tempora est vel provident voluptatem eveniet. Ad odio culpa cum eaque neque quia rem tenetur amet." 
-          />
-          <Post 
-            author="Jão Balão"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quasi sapiente cumque. Aperiam, dolore! Ipsam nam quam sunt hic quisquam accusantium voluptatibus. Error alias facere, in veniam omnis saepe praesentium?" 
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,35 +1,67 @@
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBr from 'date-fns/locale/pt-Br'
 import styles from './Post.module.css'
+import { useState } from 'react'
 
-export function Post() {
+export function Post({author, content, publishedAt }) {
+  const [comments, setComments] = useState([])
+  const [newComment, setNewComment] = useState('')
+
+  const dataTitle = format(publishedAt, "d 'de' MMMM 'às' H'h'",
+    {
+      locale: ptBr,
+    }
+  )
+  const dataDesc = formatDistanceToNow(publishedAt,
+    {
+      locale: ptBr,
+      addSuffix: true
+    }
+  )
+
+  function handleCreateNewComment() {
+    event.preventDefault()
+    setComments([...comments, newComment])
+    setNewComment('')
+  }
+  function handleNewCommentChange() {
+    setNewComment(event.target.value)
+  }
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/lemaun.png" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Leandro Silva</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
-        <time title="11 de maior às 08h15" dateTime="2022-05-11 08:15:23">Publicado há 1h</time>
+        <time title={dataTitle} dateTime={publishedAt.toISOString()}>{dataDesc}</time>
       </header>
 
       <div className={styles.content} >
-        <p>Fala galeraa 👋</p>
-
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-        <p>👉 <a href="#">jane.design/doctorcare</a></p>
-
-        <p><a href="#">#novoprojeto</a> <a href="#">#nlw</a> <a href="#">#rocketseat</a></p>
+        {content.map(line => {
+          if (line.type === 'paragraph') {
+            return <p key={line.content}>{line.content}</p>
+          } else if (line.type === 'link'){
+            return (<p key={line.content}><a href="#">{line.content}</a></p>)
+          }
+        })}
+        
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
-        <textarea placeholder="Deixe um comentário" />
+        <textarea 
+          name="comment" 
+          placeholder="Deixe um comentário"
+          onChange={handleNewCommentChange}
+          value={newComment}
+        />
 
         <footer>
           <button type="submit">Comentar</button>
@@ -37,9 +69,9 @@ export function Post() {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return <Comment key={comment} content={comment}/>
+        })}
       </div>
 
 
